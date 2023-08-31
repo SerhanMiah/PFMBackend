d769510bc53a9a533e166adc990f373cb5560af8
Serhan miah <101991709+SerhanMiah@users.noreply.github.com>
2023-08-31 10:39:07 +0100

    Local connection to database successful, able to see all tables created

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,17 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PFMBackend.Models;
 
 namespace PFMBackend.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext :  IdentityDbContext<ApplicationUser> 
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         {
 
         }
+
+
+        // DbSet
+
 
         public DbSet<AccountModel> AccountModels {get; set;}
 
@@ -25,6 +30,30 @@ namespace PFMBackend.Data
         public DbSet<TransactionModel> TransactionModels {get; set;}
 
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AccountModel>()
+            .Property(a => a.Balance)
+            .HasColumnType("decimal(18,2)"); 
+
+        modelBuilder.Entity<ApplicationUser>()
+            .Property(u => u.TotalBalance)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<BudgetModel>()
+            .Property(b => b.Amount)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<FinanceGoal>()
+            .Property(f => f.TargetAmount)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<TransactionModel>()
+            .Property(t => t.Amount)
+            .HasColumnType("decimal(18,2)");
+    }
 
 
     }
